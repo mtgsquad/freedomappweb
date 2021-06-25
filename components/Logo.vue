b1e27964bb7ca55228dab48c867df80b258204df
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-06-25 23:15:49 +0500

    Yay 🙌

---
+++ components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="https://git.opensrc.services/repo-avatars/37-623bf52cc8ad2e0f9b9aa313fc51c061" alt="Logo Component" class="bigass round">
+</template>
+
+<style>
+.round {
+  border-radius: 50%;
+}
+.bigass {
+  width: 30%;
+  height: 30%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        Freedom.
+      </h1>
+      <br>
+      <h2 class="subtitle">
+        Coming Soon.
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #35495e;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+</style>
